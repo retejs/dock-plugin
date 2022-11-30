b65d06c090aa56f3dea7cdf759aea5e1254c926c
Ni55aN <huccah@gmail.com>
2022-11-30 21:13:26 +0200

    retrieve editor and area from scope instead of props

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { BaseSchemes, NodeEditor, Scope } from 'rete'
-import { Area2DInherited, AreaPlugin } from 'rete-area-plugin'
+import { BaseSchemes, NodeEditor, Root, Scope } from 'rete'
+import { Area2D, Area2DInherited, AreaPlugin } from 'rete-area-plugin'
 
 import { ClickStrategy } from './click-strategy'
 import { DropStrategy } from './drop-strategy'
@@ -8,22 +8,24 @@ import { Strategy } from './strategy'
 
 export * as DockPresets from './presets'
 
-type Props<Schemes extends BaseSchemes, K> = {
-    editor: NodeEditor<Schemes>
-    area: AreaPlugin<Schemes, K>
-}
-
-export class DockPlugin<Schemes extends BaseSchemes, K> extends Scope<never, Area2DInherited<Schemes, never>> {
+export class DockPlugin<Schemes extends BaseSchemes> extends Scope<never, Area2DInherited<Schemes, never>> {
     nodes: Schemes['Node'][] = []
-    clickStrategy: Strategy
-    dropStrategy: Strategy
+    clickStrategy!: Strategy
+    dropStrategy!: Strategy
     presets: Preset[] = []
 
-    constructor(props: Props<Schemes, K>) {
+    constructor() {
         super('dock')
+    }
 
-        this.clickStrategy = new ClickStrategy(props.editor, props.area)
-        this.dropStrategy = new DropStrategy(props.editor, props.area)
+    setParent(scope: Scope<Area2D<Schemes>, [Root<Schemes>]>): void {
+        super.setParent(scope)
+
+        const area = this.parentScope<AreaPlugin<Schemes>>(AreaPlugin)
+        const editor = area.parentScope<NodeEditor<Schemes>>(NodeEditor)
+
+        this.clickStrategy = new ClickStrategy(editor, area)
+        this.dropStrategy = new DropStrategy(editor, area)
     }
 
     add(create: () => Schemes['Node']) {
@@ -37,16 +39,16 @@ export class DockPlugin<Schemes extends BaseSchemes, K> extends Scope<never, Are
         const element = preset.createItem()
 
         this.parentScope()?.emit({
-            type: 'render',
-            data: {
-                type: 'node',
-                element,
+                type: 'render',
+                data: {
+                    type: 'node',
+                    element,
                 payload: node
-            }
-        })
+                }
+            })
 
-        this.clickStrategy.add(element, create)
-        this.dropStrategy.add(element, create)
+            this.clickStrategy.add(element, create)
+            this.dropStrategy.add(element, create)
     }
 
     addPreset(preset: Preset) {
