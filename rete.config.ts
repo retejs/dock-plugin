810b12bf1925412169ecd188e8c9e8f4376cc25e
Ni55aN <huccah@gmail.com>
2023-04-10 14:50:28 +0300

    v2.0.0-beta.4

--- rete.config.ts
+++ rete.config.ts
@@ -3,7 +3,7 @@ import { ReteOptions } from 'rete-cli'
 
 export default <ReteOptions>{
   input: 'src/index.ts',
-  name: 'DockPlugin',
+  name: 'ReteDockPlugin',
   globals: {
     'rete': 'Rete',
     'rete-area-plugin': 'ReteAreaPlugin'
